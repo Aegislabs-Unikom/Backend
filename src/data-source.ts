161040c43de486713813e87f27c5528d0dbcff9e
zulnurdiana <zulnurdiana@gmail.com>
2023-09-25 19:40:34 +0700

    add vercel.json

--- src/data-source.ts
+++ src/data-source.ts
@@ -6,6 +6,7 @@ import { OTP } from "./entity/OTP"
 export const AppDataSource = new DataSource({
     type: "mongodb",
     database: "questfinal",
+    url: "mongodb://zulnurdiana:0GCkJ5UJPfuA4N8A@ac-dr9rqg0-shard-00-00.w67zqzg.mongodb.net:27017,ac-dr9rqg0-shard-00-01.w67zqzg.mongodb.net:27017,ac-dr9rqg0-shard-00-02.w67zqzg.mongodb.net:27017/?ssl=true&replicaSet=atlas-ial56o-shard-0&authSource=admin&retryWrites=true&w=majority",
     synchronize: true,
     logging: false,
     entities: [User,OTP],
